de041a6a33f861de681d6ef4d170223924e75365
jesse.caldas <jesse.caldas@GEOPROC-2>
2023-05-29 09:47:08 -0300

    Adicionar arquivos de projeto.

---
+++ 01Generics/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## Generics ##\n");
+
+Teste teste = new Teste();
+
+int i1 = 10;
+int i2 = 10;
+
+teste.Comparar(i1, i2);
+
+
+public class Teste
+{
+    public void Comparar( int p1,  int p2 )
+    {
+        var resultado = p1.Equals(p2);
+        /*Equals é similar ao "==", porém no Equals é comparado o conteúdo eo tipo de dados, enquanto no "==" é comparado as referências do objeto.*/
+        Console.WriteLine($"{p1} e {p2} são iguais? {resultado}");
+    }
+}
