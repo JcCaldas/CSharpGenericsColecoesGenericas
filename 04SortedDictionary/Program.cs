26a01cdae48d568fe4e646b4d22888a05b7287b1
Jessé Caldas <jessencaldas@hotmail.com>
2023-06-05 17:02:46 -0300

    add 04

---
+++ 04SortedDictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Sorted Dictionary\n");
+
+//Criando uma coleçaõ SortedDictionary. Independente da ordem que for colocado, ela sempre será ordenada pela chave automaticamente.
+
+Console.WriteLine("Criando SortedDictionary\n");
+
+var brics = new SortedDictionary<int, string>()
+{
+    {20, "Brasil" },
+    {10, "China" },
+    {40, "India" },
+    {30, "Rússia" }
+};
+
+ExibirSortedDictionary(brics);
+
+Console.WriteLine("Incluindo o elemento de chave \"70\" e valor \"África do Sul\" ");
+
+brics.Add(70, "África do Sul");
+
+ExibirSortedDictionary(brics);
+
+//Função para exibição
+static void ExibirSortedDictionary(SortedDictionary<int, string> brics)
+{
+    foreach (var item in brics)
+    {
+        Console.WriteLine($"Chave: {item.Key}\t Valor: {item.Value}");
+
+    }
+    Console.WriteLine("\n-----------------------------------\n");
+}
+
