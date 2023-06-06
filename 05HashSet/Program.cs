2f3150676a73b772336e5d040cca8bce2c47a91c
Jessé Caldas <jessencaldas@hotmail.com>
2023-06-06 17:07:58 -0300

    Add 05 e 05_1

---
+++ 05HashSet/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("HashSet\n");
+
+//criação coleção hashset com itens
+var numeros = new HashSet<int>() { 1,2,3,4,5 }; //cria um hashset de int
+
+var frutas = new HashSet<string>() { "uva", "banana", "pera" }; //cria um hashset de strings
+
+HashSet<int> pares = new HashSet<int>(); //cria um hashset vazio para ser preenchido
+
+//preenchimento do hashset pares
+for (int i = 0; i < 6; i++)
+{
+    pares.Add(i * 2);
+}
+
+//contrutor usando IEnumerable - qualquer coleção que exista e implementa IEnumerable pode ser utilizada no construtor
+var lista1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7};
+var conjunto = new HashSet<int>(lista1);
+
+var lista2 = new List<string>() { "uva", "banana", "pera" };
+HashSet<string> minhasFrutas = new HashSet<string>(lista2);
+
+var impares = new int[] { 1, 3, 5, 7, 9, 11, 13 };
+HashSet<int> meuHashset = new HashSet<int>(impares);
---
+++ 05_1HashSet/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("HashSet\n");
+
+var timesSP = new HashSet<string>() { "Santos", "Palmeiras", "São Paulo" };
+
+var timesRJ = new HashSet<string>() { "Vasco", "Flamengo", "Fluminense" };
+
+var timesBA = new HashSet<string>() { "Bahia", "Vitória", "Juazeiro" };
+
+var timesMundiais = new HashSet<string>() { "Santos", "Palmeiras", "São Paulo", "Flamengo" };
+
+//Adicionando elementos. Obs: O método Add não adiciona elementos repetidos.
+if (!timesSP.Contains("Corinthians"))
+{
+    timesSP.Add("Corinthians");
+    timesSP.Add("Santos"); //Esse elemento não será adicionado pois já existe na coleção
+}
+
+foreach (var times in timesSP)
+{
+    Console.WriteLine(times);
+}
