facb8600e6c1b5414445755e8144e76050bcd874
Jessé Caldas <jessencaldas@hotmail.com>
2023-06-07 14:51:36 -0300

    add 06

---
+++ 06SortedSet/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("SortedSet\n");
+
+//Criação de SortedSet é similar a HashSet. SortedSet sempre coloca na ordem
+
+var numeros = new SortedSet<int>() { 1, 5, 3, 4, 2 };
+ExibirColecao(numeros);
+
+var frutas = new SortedSet<string> { "uva", "banana", "pera", "kiwi" };
+ExibirColecao(frutas);
+
+SortedSet<int> pares = new SortedSet<int>();
+for (int i = 6; i > 0; i--)
+{
+    pares.Add(i*2);
+}
+ExibirColecao(pares);
+
+//pode-se criar a partir de uma lista.
+
+var numerosLista = new List<int>() { 1, 7, 3, 6, 5, 4, 2 };
+var conjunto = new SortedSet<int>(numerosLista);
+ExibirColecao(conjunto);
+
+static void ExibirColecao<T>(IEnumerable<T> colecao)
+{
+    Console.WriteLine("\n---------------------------------\n");
+    foreach (var item in colecao)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine("\n---------------------------------\n");
+}
